8197825085b888bfecc328adabbafa26ea6fc2d7
Soham Datta <83786816+TECH-SAVVY-GUY@users.noreply.github.com>
2023-12-16 16:48:07 +0530

    Update `server.py`

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
 import os
 import flask
 from flask import request
+from flask import redirect
 
 import telebot
 from telebot.util import parse_web_app_data
 from telebot.util import validate_web_app_data
+from telebot.types import InputTextMessageContent
+from telebot.types import InlineQueryResultArticle
 from telebot.types import InlineQueryResultCachedPhoto
 
 API_TOKEN = os.getenv("API_TOKEN")
 STORAGE_CHANNEL = os.getenv("STORAGE_CHANNEL")
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_url_path="static")
 bot = telebot.TeleBot(API_TOKEN, parse_mode="HTML")
 
 from io import BytesIO
@@ -24,6 +27,10 @@ def generate_qr(data):
 
 @app.route('/')
 def index():
+    return redirect('https://t.me/QRCodeTelegramBot')
+
+@app.route('/qrCodeEvent')
+def qrCodeEvent():
     return flask.render_template("event.html")
 
 @app.route('/qrcallback', methods=["POST"])
@@ -59,6 +66,34 @@ def qrcallback():
 		bot.answer_web_app_query(query_id, InlineQueryResultCachedPhoto(query_id, file_id))
 
 	return "200"
+
+# ------------------- QR Code Scanner ------------------- #
+
+@app.route('/qrCode')
+def qrCode():
+    return flask.render_template("qrCode.html")
+
+@app.route('/qrCodeResponse', methods=["POST"])
+def qrCodeResponse():
+    raw_data = flask.request.json
+    initData = raw_data["initData"]
+
+    isValid = validate_web_app_data(API_TOKEN, initData)
+
+    if isValid:
+        web_app_data = parse_web_app_data(API_TOKEN, initData)
+
+        query_id = web_app_data["query_id"]
+
+        bot.answer_web_app_query(query_id, InlineQueryResultArticle(
+            id=query_id, title="QR DETECTED!",
+            input_message_content=InputTextMessageContent(
+                f"<i>QR Code scanned successfully! 👇🏻\n\
+				\n{raw_data['qr']}</i>", parse_mode="HTML")))
+
+    return redirect("/")
+
+# ------------------- QR Code Scanner ------------------- #
 
 if __name__ == '__main__':
     app.run()
